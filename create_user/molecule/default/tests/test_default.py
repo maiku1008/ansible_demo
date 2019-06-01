65fce6c951e5120a2a0fb008173264f137e159f7
Michael C <rm.cuffaro@gmail.com>
2019-06-01 21:33:32 +0200

    feat: add role with tests!

---
+++ create_user/molecule/default/tests/test_default.py
@@ -0,0 +1,24 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_user(host):
+    f = host.user('pippo')
+
+    assert f.exists
+
+
+def test_group(host):
+    f = host.group('pippo')
+
+    assert f.exists
+
+
+def test_file(host):
+    f = host.file('/tmp/pippo')
+
+    assert not f.exists
